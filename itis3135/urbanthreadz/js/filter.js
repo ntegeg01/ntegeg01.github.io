e919126f69b36a3ae1ab5daaf12d35b05211cf85
Nebiyu <ntegegne@uncc.edu>
2025-12-07 21:04:45 -0500

    chanhes

--- itis3135/urbanthreadz/js/filter.js
+++ itis3135/urbanthreadz/js/filter.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
     searchInput.addEventListener("keyup", () => {
       let value = searchInput.value.toLowerCase();
   
-      items.forEach(card => {
+      items.forEach((card) => {
         let name = card.querySelector(".product-name").textContent.toLowerCase();
         card.style.display = name.includes(value) ? "block" : "none";
       });
